aab329cbd6f8ac86e7bc64ce6163c6d71796ccb1
Ram Iyer <ramky.iyer@yahoo.com>
2016-03-16 19:33:22 -0500

    added test to show an uploaded image on the products index page

--- test/acceptance/products_test.exs
+++ test/acceptance/products_test.exs
@@ -6,14 +6,17 @@ defmodule PhoenixEcommerce.Acceptance.ProductsTest do
   alias PhoenixEcommerce.Product
   alias PhoenixEcommerce.Repo
 
+  @upload %Plug.Upload{ path: Path.relative_to_cwd("test/files/elixir.png"), filename: "elixir.png", content_type: "image/png" }
+
   setup do
     Repo.delete_all(Product)
     {:ok, product} =
-      %Product{
+      Product.changeset(%Product{}, %{
         name: "Some product",
         description: "A very long description for my product",
-        price: Decimal.new("25.20")
-      } |> Repo.insert
+        price: Decimal.new("25.20"),
+        image: @upload
+      }) |> Repo.insert
 
     {:ok, product: product}
   end
@@ -23,17 +26,19 @@ defmodule PhoenixEcommerce.Acceptance.ProductsTest do
     assert find_element(:css, "ul.products")
   end
 
-  test "/products - product details include name, description, and price", %{product: product} do
+  test "/products - product details include name, description, price and image", %{product: product} do
 
     navigate_to "/products"
     product_li  = find_element(:css, "ul.products li")
     name        = find_within_element(product_li, :css, "header")
     price       = find_within_element(product_li, :css, "h4")
     description = find_within_element(product_li, :css, "p")
+    image       = find_within_element(product_li, :css, "img")
 
     assert visible_text(name)        == product.name
     assert visible_text(price)       == "$#{product.price}"
     assert visible_text(description) == product.description
+    assert attribute_value(image, "src") =~  ~r/amazon/
   end
 
   test "/products - click a product to view its details", %{product: product} do
